276910925806e203fade7496b9c1a07633d554a8
developerhizol <kostatrofimcev11@gmail.com>
2026-09-05 11:06:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ from handlers.payment import PLANS, user_transactions, user_payment, user_paymen
 logging.basicConfig(level=logging.ERROR)
 logger = logging.getLogger(__name__)
 
-PORT = 7529
+PORT = 6192
 SUBSCRIPTION_DOMAIN = "streamnetvpn.bothost.tech"
 ADMIN_ID = 7752488661
 
